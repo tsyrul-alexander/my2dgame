72df97307fb911cf7b1193d723d3449ee64caeb1
tsyrul-alexander <tsyrulalexander@gmail.com>
2020-07-10 14:32:25 +0300

    #0 Initialize structure

---
+++ My2DGame.Core/Component/IComponent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace My2DGame.Core.Component
+{
+	public interface IComponent
+	{
+	}
+}
---
+++ My2DGame.Core/GameObject/IGameObject.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace My2DGame.Core.GameObject
+{
+	public interface IGameObject
+	{
+	}
+}
